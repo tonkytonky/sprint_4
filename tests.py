536253b334b2362da6f0d951e0e408185ccdcc9d
Anton Samsonov <a.samsonov@gardatech.ru>
2024-03-27 23:28:21 +0300

    Add first tests

--- tests.py
+++ tests.py
@@ -1,24 +1,153 @@
 from main import BooksCollector
+from random import choice
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    book_name1 = "Гордость и предубеждение и зомби"
+    book_name2 = "Что делать, если ваш кот симулирует"
+    book_name3 = "Перфорация на перфокартах как искусство"
+    book_name_too_long = "VERY_VERY_VERY_VERY_VERY_VERY_VERY_LONG_NAME"
+    genres_valid = ["Фантастика", "Ужасы", "Детективы", "Мультфильмы", "Комедии"]
+    invalid_genre = "INVALID_GENRE"
+    genres_age_rating = ["Ужасы", "Детективы"]
+    genres_children = list(set(genres_valid) - set(genres_age_rating))
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_add_valid_name(self, collector: BooksCollector):
+        """
+        Добавление книги в коллекцию. Название допустимой длинны.
+        Книга добавлена с пустым жанром.
+        """
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        collector.add_new_book(name=self.book_name1)
+        
+        books_genre = collector.get_books_genre()
+        assert books_genre[self.book_name1] == ""
+        assert len(books_genre) == 1
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_invalid_name(self, collector: BooksCollector):
+        """
+        Добавление книги в коллекцию. Название превышает допустимую длину.
+        Книга не добавлена.
+        """
+
+        collector.add_new_book(name=self.book_name_too_long)
+
+        books_genre = collector.get_books_genre()
+        assert len(books_genre) == 0
+
+    def test_set_book_genre_valid_genre(self, collector: BooksCollector):
+        """
+        Установка жанра из списка допустимых.
+        Жанр устанавливается.
+        """
+
+        collector.add_new_book(name=self.book_name1)
+        genre_valid = choice(self.genres_valid)
+        collector.set_book_genre(name=self.book_name1, genre=genre_valid)
+
+        books_genre = collector.get_books_genre()
+        assert books_genre[self.book_name1] == genre_valid
+    
+    def test_set_book_genre_invalid_genre(self, collector: BooksCollector):
+        """
+        Установка жанра не из списка допустимых.
+        Жанр остаётся пустым.
+        """
+
+        collector.add_new_book(name=self.book_name1)
+        collector.set_book_genre(name=self.book_name1, genre=self.invalid_genre)
+
+        books_genre = collector.get_books_genre()
+        assert books_genre[self.book_name1] == ""
+
+    def test_get_book_genre(self, collector: BooksCollector):
+        """
+        Получение жанра книги по её названию.
+        Возвращается установленный по названию книги жанр.
+        """
+
+        collector.add_new_book(name=self.book_name1)
+        genre_valid = choice(self.genres_valid)
+        collector.set_book_genre(name=self.book_name1, genre=genre_valid)
+
+        actual_genre = collector.get_book_genre(name=self.book_name1)
+        assert actual_genre == genre_valid
+
+    def test_get_books_with_specific_genre(self, collector: BooksCollector):
+        """
+        Получение списка книг с заданным жанром. 
+        Возвращаются только книги с искомым жанром.
+        """
+        
+        collector.add_new_book(name=self.book_name1)
+        collector.add_new_book(name=self.book_name2)
+        collector.add_new_book(name=self.book_name3)
+        genre_valid1 = self.genres_valid[0]
+        genre_valid2 = self.genres_valid[1]
+        collector.set_book_genre(name=self.book_name1, genre=genre_valid1)
+        collector.set_book_genre(name=self.book_name2, genre=genre_valid1)
+        collector.set_book_genre(name=self.book_name3, genre=genre_valid2)
+
+        books = collector.get_books_with_specific_genre(genre=genre_valid1)
+        assert len(books) == 2
+
+    def test_get_books_genre(self, collector: BooksCollector):
+        """
+        Получение коллекции книг.
+        Возвращается словарь содержащий "Название книги: жанр"
+        """
+        
+        collector.add_new_book(name=self.book_name1)
+        genre_valid = choice(self.genres_valid)
+        collector.set_book_genre(name=self.book_name1, genre=genre_valid)
+
+        books_genre = collector.get_books_genre()
+        book1_genre = books_genre.get(self.book_name1)
+        assert book1_genre is not None
+        assert book1_genre == genre_valid
+
+    def test_get_books_for_children(self, collector: BooksCollector):
+        """
+        Получение книг для детей.
+        Возвращаются только книги с жанром без ограничения.
+        """
+        
+        collector.add_new_book(name=self.book_name1)
+        collector.add_new_book(name=self.book_name2)
+        genre_age_rating = choice(self.genres_age_rating)
+        genre_children = choice(self.genres_children)
+        collector.set_book_genre(name=self.book_name1, genre=genre_age_rating)
+        collector.set_book_genre(name=self.book_name2, genre=genre_children)
+
+        books = collector.get_books_for_children()
+        assert len(books) == 1
+
+    def test_add_book_in_favorites_add_book_only_once(self, collector: BooksCollector):
+        """
+        Добавление в Избранное.
+        Книгу можно добавить только один раз.
+        """
+        
+        collector.add_new_book(name=self.book_name1)
+        collector.add_book_in_favorites(name=self.book_name1)
+        collector.add_book_in_favorites(name=self.book_name1)
+
+        books = collector.get_list_of_favorites_books()
+        assert len(books) == 1
+
+    def test_delete_book_from_favorites(self, collector: BooksCollector):
+        """
+        Удаление из Избранного.
+        Удаляется только одно книга.
+        """
+        
+        collector.add_new_book(name=self.book_name1)
+        collector.add_new_book(name=self.book_name2)
+        collector.add_book_in_favorites(name=self.book_name1)
+        collector.add_book_in_favorites(name=self.book_name2)
+        collector.delete_book_from_favorites(name=self.book_name1)
+
+        books = collector.get_list_of_favorites_books()
+        assert len(books) == 1
+        assert self.book_name2 in books
